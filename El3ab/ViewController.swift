83095cab3be4b11146b913b5b833d9e6c31c3a14
osama <Osamakhaled31415@gmail.com>
2026-06-04 15:07:49 +0300

    Initial Commit

---
+++ El3ab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  El3ab
+//
+//  Created by Osama Khaled on 04/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
