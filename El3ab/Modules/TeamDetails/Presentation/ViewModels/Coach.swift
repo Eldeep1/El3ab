698782b3a0e81481400e69873fcc705a486a7216
osama <Osamakhaled31415@gmail.com>
2026-06-06 18:57:09 +0300

    Team Details Screen UI

---
+++ El3ab/Modules/TeamDetails/Presentation/ViewModels/Coach.swift
@@ -0,0 +1,14 @@
+//
+//  Coach.swift
+//  El3ab
+//
+//  Created by Osama Khaled on 06/06/2026.
+//
+
+import Foundation
+
+struct Coach {
+    let name: String
+    let imageName: String
+    let role: String
+}
---
+++ El3ab/Modules/TeamDetails/Presentation/ViewModels/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  El3ab
+//
+//  Created by Osama Khaled on 06/06/2026.
+//
+
+import Foundation
+struct Player {
+    let name: String
+    let imageName: String
+    let position: String?
+    let number: Int?
+}
+
