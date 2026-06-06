289c1ff9b0fc9638602600864518942252d954fb
osama <Osamakhaled31415@gmail.com>
2026-06-06 19:47:16 +0300

    add package2

---
+++ El3ab/Modules/TeamDetails/Presentation/ViewModels/Team.swift
@@ -0,0 +1,16 @@
+//
+//  Team.swift
+//  El3ab
+//
+//  Created by Osama Khaled on 06/06/2026.
+//
+
+import Foundation
+
+struct Team {
+    let name: String
+    let logoImageName: String
+    let players: [Player]
+    let coaches: [Coach]
+}
+
