f393f1616743f231aa3fc1d692979acb04ee87c2
depo <alieldeep111@gmail.com>
2026-06-07 23:33:06 +0300

    creating latest events initial design

---
+++ El3ab/Modules/LeagueDetails/Presentation/LatestEventsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LatestEventsCollectionViewCell.swift
+//  El3ab
+//
+//  Created by depo on 07/06/2026.
+//
+
+import UIKit
+
+class LatestEventsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
