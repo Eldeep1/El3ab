96e5dc432dc13fc0a4552c1a87aa2f5b3d2da72a
depo <alieldeep111@gmail.com>
2026-06-07 23:33:40 +0300

    creating teams cell initial design

---
+++ El3ab/Modules/LeagueDetails/Presentation/TeamsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TeamsCollectionViewCell.swift
+//  El3ab
+//
+//  Created by depo on 07/06/2026.
+//
+
+import UIKit
+
+class TeamsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
