7e73874a77727f4181518561ae42c7513ee19be2
depo <alieldeep111@gmail.com>
2026-06-07 23:32:36 +0300

    creating upcoming events design

---
+++ El3ab/Modules/LeagueDetails/Presentation/UpComingEventsViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UpComingEventsViewCell.swift
+//  El3ab
+//
+//  Created by depo on 07/06/2026.
+//
+
+import UIKit
+
+class UpComingEventsViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
